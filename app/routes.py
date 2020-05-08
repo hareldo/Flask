593c4a38d68b7055c1ab8d03d3b93e02400581b7
hareldo <44146957+hareldo@users.noreply.github.com>
2020-05-08 10:18:26 +0300

    Add files via upload

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, CodeForm
 
 @app.route('/')
 @app.route('/index')
@@ -18,3 +18,13 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/code', methods=['GET', 'POST'])
+def run_code():
+    form = CodeForm()
+    if form.validate_on_submit():
+        flash('Run Code {}, result={}'.format(
+            form.code.data, 0))
+        exec(form.code.data)
+        return redirect('/index')
+    return render_template('code.html', title='IDE', form=form)
